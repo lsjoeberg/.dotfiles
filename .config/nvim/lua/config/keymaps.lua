0469dd80173fb85805f880df0ba040073f1243fa
Lars Sjöberg <lsjoeberg@protonmail.com>
2025-10-18 23:44:13 +0200

    Format nvim config with `stylua`

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,11 +1,13 @@
 local km = vim.keymap
 
 -- Save
-km.set("n", "<leader>w", "<CMD>update<CR>", { desc = "Write buffer if it has been modified"} )
+km.set("n", "<leader>w", "<CMD>update<CR>", { desc = "Write buffer if it has been modified" })
 
 -- Quit
-km.set("n", "<leader>q", "<CMD>q<CR>", { desc = "Quit"} )
-km.set("n", "<leader>d", "<CMD>bd<CR>", { desc = "Close current buffer"} )
+km.set("n", "<leader>q", "<CMD>q<CR>", { desc = "Quit" })
+km.set("n", "<leader>d", "<CMD>bd<CR>", { desc = "Close current buffer" })
 
 -- Diagnostics
-km.set("n", "gl", function() vim.diagnostic.open_float() end, { desc = "Open diagnostics in float" })
+km.set("n", "gl", function()
+  vim.diagnostic.open_float()
+end, { desc = "Open diagnostics in float" })
