216bc41c4b76a997f4efd360e4552901924bfbf5
Lars Sjöberg <lsjoeberg@protonmail.com>
2025-10-19 17:01:09 +0200

    Fix nvim-treesitter config

--- .config/nvim/lua/plugins/treesitter-textobjects.lua
+++ .config/nvim/lua/plugins/treesitter-textobjects.lua
@@ -1,31 +1,32 @@
 return {
   "nvim-treesitter/nvim-treesitter-textobjects",
+  branch = "main",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
-  init = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      -- TODO: Rust patterns
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-          },
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            ["<leader>a"] = { query = "@parameter.inner", desc = "Swap with next parameter" },
-          },
-          swap_previous = {
-            ["<leader>A"] = { query = "@parameter.inner", desc = "Swap with previous parameter" },
-          },
+  opts = {
+    -- TODO: Rust patterns
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
         },
       },
-    })
+      swap = {
+        enable = true,
+        swap_next = {
+          ["<leader>a"] = { query = "@parameter.inner", desc = "Swap with next parameter" },
+        },
+        swap_previous = {
+          ["<leader>A"] = { query = "@parameter.inner", desc = "Swap with previous parameter" },
+        },
+      },
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter-textobjects").setup(opts)
   end,
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,21 +1,43 @@
+local languages = {
+  "bash",
+  "c",
+  "lua",
+  "markdown",
+  -- Go
+  "go",
+  "gomod",
+  "gowork",
+  "gosum",
+  "gotmpl",
+  -- Rust
+  "rust",
+  "toml",
+}
+
 return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = "master",
-  lazy = false,
-  build = ":TSUpdate",
-  opts = {
-    ensure_installed = {
-      "go",
-      "gomod",
-      "gowork",
-      "gosum",
-    },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
+    build = ":TSUpdate",
   },
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      highlight = { enable = true },
-      ident = { enable = true },
+  {
+    "MeanderingProgrammer/treesitter-modules.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    ---@module 'treesitter-modules'
+    ---@type ts.mod.UserConfig
+    opts = {
+      -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+      ensure_installed = languages,
+      -- Automatically install missing parsers when entering buffer (requires `tree-sitter` CLI installed)
+      auto_install = true,
+
+      highlight = {
+        enable = true,
+      },
+      ident = {
+        enable = true,
+      },
       incremental_selection = {
         enable = true,
         keymaps = {
@@ -25,6 +47,6 @@ return {
           node_decremental = "<Backspace>",
         },
       },
-    })
-  end,
+    },
+  },
 }
