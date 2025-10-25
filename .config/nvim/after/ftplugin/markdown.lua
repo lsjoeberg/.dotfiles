3055a4bb29a91e4205224f53e4c22b98c8e44c35
Lars Sjöberg <lsjoeberg@protonmail.com>
2025-10-25 21:36:17 +0200

    nvim: add ftplugin for mardown

---
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,7 @@
+-- Indent with 2 spaces
+vim.bo.tabstop = 2 -- visual spaces per tab
+vim.bo.shiftwidth = 2 -- number of spaces per step of autoindent
+vim.bo.softtabstop = 2 -- number of spaces in tab when editing
+
+-- Conceal
+vim.opt_local.conceallevel = 2
