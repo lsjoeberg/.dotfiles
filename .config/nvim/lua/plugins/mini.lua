c4d13916b508ce4279355c8626536dd373e7f992
Lars Sjöberg <lsjoeberg@protonmail.com>
2025-10-25 22:46:13 +0200

    nvim: remap mini.surround with <leader>

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -4,7 +4,17 @@ return {
     {
       "nvim-mini/mini.surround",
       version = "*",
-      opts = {},
+      opts = {
+        -- Remap default  bindings to start with <leader>
+        mappings = {
+          add = "<leader>sa", -- Add surrounding in Normal and Visual modes
+          delete = "<leader>sd", -- Delete surrounding
+          find = "<leader>sf", -- Find surrounding (to the right)
+          find_left = "<leader>sF", -- Find surrounding (to the left)
+          highlight = "<leader>sh", -- Highlight surrounding
+          replace = "<leader>sr", -- Replace surrounding
+        },
+      },
     },
   },
 }
